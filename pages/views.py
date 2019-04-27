7743bed589d735bf0629624d319af10c6bdf96f6
unknown <jigneshkotadiya000@gmail.com>
2019-04-27 21:11:46 +0530

    pages update

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choice, bedroom_choices, state_choice
+
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings' : listings,
+        'state_choices' : state_choice,
+        'bedroom_choices' : bedroom_choices,
+        'price_choices' : price_choice
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+
+    # get all realtores
+    realtores = Realtor.objects.order_by('-hire_date')
+
+    # get MVP
+    mvp_realtores = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtores': realtores,
+        'mvp_realtores': mvp_realtores
+    }
+    return render(request, 'pages/about.html', context)
